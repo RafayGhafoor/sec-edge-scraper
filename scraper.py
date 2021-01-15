aa8db99555d1214c6bb97cdc24631cab7c8235e6
Rafay Ghafoor <rafayghafoor@pm.me>
2021-01-15 15:14:51 +0500

    feat: Bump, developed prototype

--- scraper.py
+++ scraper.py
@@ -157,22 +157,29 @@ async def grabber(s, url, _cik, _date):
 async def main():
     s = Session(connections=50)
     input_data = read_csv()
-    # with alive_bar(len(input_data), title="Total Progress") as total_bar:
-    for _cik, _date in input_data:
-        print(f"Testing {_cik}")
-        start_date, end_date = custom_date_generator(_date)
-        extracted_urls = await make_search(s, _cik, start_date, end_date)
-        extracted_urls = [i for i in extracted_urls if i.endswith('txt')]
-        text_date = date_to_text(_date)
-        kp.add_keywords_from_list(list(keywords))
-        kp.add_keyword(text_date)
-        _urls_length = len(extracted_urls)
-        if not _urls_length:
-            continue
-        async with trio.open_nursery() as n:
-            for url in extracted_urls:
-                n.start_soon(grabber, s, url, _cik, date_to_fn(_date))
-        kp.remove_keyword(_date)
-            # total_bar()
-    progress_writer.close()
+    try:
+        with alive_bar(len(input_data), title="Total Progress") as total_bar:
+            for _cik, _date in input_data:
+                try:
+                    start_date, end_date = custom_date_generator(_date)
+                    extracted_urls = await make_search(s, _cik, start_date, end_date)
+                    extracted_urls = [i for i in extracted_urls if i.endswith('txt')]
+                    text_date = date_to_text(_date)
+                    kp.add_keywords_from_list(list(keywords))
+                    kp.add_keyword(text_date)
+                    _urls_length = len(extracted_urls)
+                    if not _urls_length:
+                        continue
+                    async with trio.open_nursery() as n:
+                        for url in extracted_urls:
+                            n.start_soon(grabber, s, url, _cik, date_to_fn(_date))
+                    kp.remove_keyword(_date)
+                except Exception as e:
+                    pass
+                finally:
+                    total_bar()
+    except Exception as e:
+        print(e)
+    finally:
+        progress_writer.close()
 trio.run(main)
