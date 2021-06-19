2cc43d237f66071a9559eac21eca0bfc35e2d434
Rafay Ghafoor <rafayghafoor@pm.me>
2021-06-20 04:58:36 +0500

    Added support for output

--- extractor.py
+++ extractor.py
@@ -33,9 +33,18 @@ class ParseAgreement:
         collected_data = {}
 
         for index, i in enumerate(self.content):
-            if 'covenant' in i.lower() sum(c.isdigit() for c in i) and len([c for c in i if c.isdigit()]) <= 3 or 'Affirmative Covenants'.lower() in i.lower() or 'Negative Covenants'.lower() in i.lower() or "article" in self.content[index-1].lower() or "section" in self.content[index-1].lower():
-                headings_mapping[i] = []
-                section_number = float(
+            if not 'covenant' in i.lower():
+                continue
+            print(i, self.content[index-1].lower(), self.content[index-2].lower(), self.content[index-3].lower())
+# Look for section number upwards...
+            if (sum(c.isdigit() for c in i) and len([c for c in i if c.isdigit()]) <= 3) or "article" in " ".join([self.content[index-1].lower(), self.content[index-2].lower(), self.content[index-3].lower()]) or "section" in self.content[index-1].lower():
+                headings_mapping[i]= []
+                print('h', i)
+                input()
+                if 'article' in i:
+                    print("HERE")
+                    input()
+                section_number= float(
                     utils.get_section_number(i))
                 for line in range(index+1, index + 100):
                     try:
@@ -45,7 +54,7 @@ class ParseAgreement:
                         if not self.content[line].strip():
                             continue
 
-                        section_number_now = float(utils.get_section_number(
+                        section_number_now= float(utils.get_section_number(
                             self.content[line]))
                         if section_number != -1 and section_number_now != -1:
                             # print(section_number_now, section_number, self.content[line])
@@ -67,28 +76,28 @@ class ParseAgreement:
         if headings_mapping:
             print("Running on file: ", filename)
 
-        cache = set()
+        cache= set()
 
-        values_collection = []
+        values_collection= []
         for k, v in headings_mapping.items():
             # if k.startswith(' '): continue
-            key = ' '.join(k.strip().split()).strip()
+            key= ' '.join(k.strip().split()).strip()
             try:
-                section_key = [i for i in key.split() if i[0].isdigit()
+                section_key= [i for i in key.split() if i[0].isdigit()
                                ][0].replace('.', '')
                 if len(section_key) > 2:
                     continue
             except Exception as e:
                 print(e)
             finally:
-                found_values = []
-                key = ' '.join(key.split()).strip()
+                found_values= []
+                key= ' '.join(key.split()).strip()
                 if key not in cache and '\n' not in key and len(key) > 8 and key[-1].isdigit():
                     found_values.append(key)
                 cache.add(key)
 
                 for i in v:
-                    val = ' '.join(i.split())
+                    val= ' '.join(i.split())
                     if val not in cache and '\n' not in val and len(val) > 8 and val[-1].isdigit():
                         found_values.append(val)
                     cache.add(val)
@@ -110,27 +119,27 @@ def is_renegotiated(content):
 
 def get_agreement_info(data):
     global TOTAL
-    result = ""
-    contents = data[100:]  # skip first 100 lines
-    header = ' '.join(data[:100]).lower()
-    agreement_phrase = [line for line in data[:100]
+    result= ""
+    contents= data[100:]  # skip first 100 lines
+    header= ' '.join(data[:100]).lower()
+    agreement_phrase= [line for line in data[:100]
                         if 'amended' in line.lower() or 'restated' in line.lower()]
-    should_run = 'amended' in header or 'restated' in header
+    should_run= 'amended' in header or 'restated' in header
     if not agreement_phrase:
         return ['', '']
 
-    data = []  # Line Number, data
+    data= []  # Line Number, data
 
     for line_no, content in enumerate(contents):
         if re.findall('.*dated as of.*', content):
             data.append((line_no, content))
 
-    statement = ""
+    statement= ""
 
     try:
-        line_no, content = data[1]
+        line_no, content= data[1]
 
-        threshold = 5
+        threshold= 5
 
         # Add previous lines
         for i in range(line_no-threshold, line_no):
@@ -144,7 +153,7 @@ def get_agreement_info(data):
             #     break
 
         if 'existing' in statement.lower():
-            filtered_statement = statement[statement.lower().find(
+            filtered_statement= statement[statement.lower().find(
                 'whereas'):].strip()
             if not filtered_statement:
                 result += statement + '\n'
@@ -155,14 +164,14 @@ def get_agreement_info(data):
         print(e)
         pass
 
-    returned_elements = ['', result]
+    returned_elements= ['', result]
     if agreement_phrase:
-        returned_elements[0] = ' '.join(agreement_phrase[:2])
+        returned_elements[0]= ' '.join(agreement_phrase[:2])
     return tuple(returned_elements)
 
 
 def parse_file(content):
-    soup = bs4.BeautifulSoup(content, 'lxml')
+    soup= bs4.BeautifulSoup(content, 'lxml')
     return soup
 
 
@@ -170,10 +179,10 @@ def main():
     # os.chdir('resources')
     os.chdir('data')
     # os.chdir('covenants')
-    json_data = []
-    count = 1
-    cwd_files = os.listdir('.')
-    total_files = len(cwd_files)
+    json_data= []
+    count= 1
+    cwd_files= os.listdir('.')
+    total_files= len(cwd_files)
     for num, _file in enumerate(cwd_files, 1):
         print(f"Processing {_file}: [{num}/{len(cwd_files)}]")
         if not _file.endswith('.txt'):
@@ -181,19 +190,19 @@ def main():
         if not _file.endswith('0000203248_08012003.txt'):
             continue
 
-        cik, date = _file.replace('.txt', '').strip().split('_')
-        cik = str(int(cik))
-        month, date, year = date[0:2], date[2:4], date[4:]
-        formatted_date = f"{month}/{date}/{year}"
+        cik, date= _file.replace('.txt', '').strip().split('_')
+        cik= str(int(cik))
+        month, date, year= date[0:2], date[2:4], date[4:]
+        formatted_date= f"{month}/{date}/{year}"
 
         # if _file != '0001163302_12152005.txt': continue
         with open(_file, 'r') as f:
             try:
 
-                file_content = f.read()
+                file_content= f.read()
 
-                agreement_parser = ParseAgreement(file_content)
-                filename, values = agreement_parser.get_covenant_categories(
+                agreement_parser= ParseAgreement(file_content)
+                filename, values= agreement_parser.get_covenant_categories(
                     _file)
                 if values:
                     json_data.append({filename: values})
